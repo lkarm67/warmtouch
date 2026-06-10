7bf4de88916abd324392ac991703e57c2fa9bff6
Liudmyla <lkarm67@gmail.com>
2026-06-10 20:18:13 +0300

    add images, fix about

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image';
 export default function About() {
     return (
         <section className={css.about + ' container'}>
-            <div className={css.aboutImage}>
+            <div className={css.imageWrapper}>
                 <Image 
-                    src="/about.jpg" 
+                    src="/images/about_td.jpeg" 
                     alt="Григорій Коротя" 
-                    width={400} height={400} 
-                    className={css.profileImage} 
+                    width={400}
+                    height={500}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 40vw" 
+                    className={css.aboutImage} 
                 />
             </div>
             <div className={css.aboutContent}>
@@ -17,93 +19,126 @@ export default function About() {
                 <div className={css.dividerContainer}>
                     <hr className={css.divider}/>
                     <svg className={css.icon_fire}>
-                        <use href="#fire-icon"></use>
+                        <use href="/icons.svg#icon-fire"></use>
                     </svg> 
                     <hr className={css.divider}/>
                 </div>
                 <p className={css.description}>
                      Понад 10 років займаюся будівництвом печей, камінів, груб та барбекю-комплексів під ключ. 
+                     Кожен проєкт виконується індивідуально з урахуванням ваших побажань, особливостей приміщення та бюджету. 
+                     Моя мета — щоб піч або камін дарували тепло, затишок та служили вашій родині багато років.
                 </p>
                 {/* <p className={css.description}>
                      У своїй роботі я поєдную перевірені часом технології пічного ремесла з сучасними матеріалами та вимогами до безпеки. 
                      Для мене важливо не лише створити надійну конструкцію, а й зробити її гармонійною частиною вашого дому.
-                </p>
-                <p className={css.description}>
-                     Кожен проєкт виконується індивідуально з урахуванням ваших побажань, особливостей приміщення та бюджету. 
-                     Моя мета — щоб піч або камін дарували тепло, затишок та служили вашій родині багато років.
                 </p>*/}
                 
                 <ul className={css.infoList}>
                     <li className={css.infoItem}>
-                        <svg className={css.calendar}>
-                            <use href="#calendar-icon"></use>
+                        <svg className={css.aboutIcon}>
+                            <use href="/icons.svg#icon-hour-glass1"></use>
                         </svg>
                         <div className={css.infoText}>
-                            <h2 className={css.infoNumber}>10+</h2>
+                            <h3 className={css.infoSubtitle}>10+</h3>
                             <p className={css.infoLabel}>років досвіду</p>
                         </div>
                     </li>
                     <li className={css.infoItem}>
-                        <svg className={css.pechi}>
-                            <use href="#pechi-icon"></use>
+                        <svg className={css.aboutIcon}>
+                            <use href="/icons.svg#icon-hammer"></use>
                         </svg>
                         <div className={css.infoText}>
-                            <h2 className={css.infoSubtitle}>50+</h2>
+                            <h3 className={css.infoSubtitle}>50+</h3>
                             <p className={css.infoLabel}>виконаних проєктів</p>
                         </div>
                     </li>
                     <li className={css.infoItem}>
-                        <svg className={css.people}>
-                            <use href="#people-icon"></use>
+                        <svg className={css.aboutIcon}>
+                            <use href="/icons.svg#icon-users1"></use>
                         </svg>
                         <div className={css.infoText}>
-                            <h2 className={css.infoSubtitle}>98%</h2>
+                            <h3 className={css.infoSubtitle}>98%</h3>
                             <p className={css.infoLabel}>задоволених клієнтів</p>
                         </div>
                     </li>
                     <li className={css.infoItem}>
-                        <svg className={css.schit}>
-                            <use href="#schit-icon"></use>
+                        <svg className={css.aboutIcon}>
+                            <use href="/icons.svg#icon-shield1"></use>
                         </svg>
                         <div className={css.infoText}>
-                            <h2 className={css.infoSubtitle}>Гарантія</h2>
+                            <h3 className={css.infoSubtitle}>Гарантія</h3>
                             <p className={css.infoLabel}>на всі роботи</p>
                         </div>
                     </li>
                 </ul>    
 
                 <div className={css.infoAdvantages}>
-                    <h2 className={css.infoAdvantagesTitle}>Чому мені довіряють</h2>
-                    <div>
+                    <h3 className={css.infoAdvantagesTitle}>Чому мені довіряють</h3>
+                    <div className={css.infoAdvantageBlock}>
                         <ul className={css.infoAdvantagesList}>
                             <li className={css.infoAdvantageItem}>
                                 <svg className={css.infoIcon}>
-                                    <use href="#project-icon"></use>
+                                    <use href="/icons.svg#icon-check1"></use>
                                 </svg>
-                                Індивідуальний проєкт</li>
+                                <p className={css.infoAdvantageText}>
+                                    Індивідуальний проєкт
+                                </p>
+                            </li>
                             <li className={css.infoAdvantageItem}>
                                 <svg className={css.infoIcon}>
-                                    <use href="#materials-icon"></use>
+                                    <use href="/icons.svg#icon-check1"></use>
                                 </svg>
-                                Якісні матеріали</li>
+                                <p className={css.infoAdvantageText}>
+                                    Якісні матеріали
+                                </p>
+                            </li>
+                        </ul>
+
+                        <div className={css.infoTrustReasons}>
+                            
+                                <svg className={css.qouteIcon}>
+                                    <use href="/icons.svg#icon-quotes-left"></use>
+                                </svg>
+                                <p className={css.infoTrustReason}>
+                                Кожна піч, камін чи барбекю-комплекс — це не просто будівництво, а створення місця, де збирається родина. 
+                                Саме тому я приділяю особливу увагу якості матеріалів, безпеці конструкції та акуратності виконання робіт.
+                                </p>
+                                <svg className={css.qouteIcon}>
+                                    <use href="/icons.svg#icon-quotes-right"></use>
+                                </svg>
+                            
+                        </div>
+
+                    </div>
+
+                    <div className={css.infoAdvantageBlock}>
+                        <ul className={css.infoAdvantagesList}>
                             <li className={css.infoAdvantageItem}>
                                 <svg className={css.infoIcon}>
-                                    <use href="#timeline-icon"></use>
+                                    <use href="/icons.svg#icon-check1"></use>
                                 </svg>
-                                Дотримання термінів</li>
+                                <p className={css.infoAdvantageText}>
+                                    Дотримання термінів
+                                </p>
+                            </li>
                             <li className={css.infoAdvantageItem}>
                                 <svg className={css.infoIcon}>
-                                    <use href="#consultation-icon"></use>
+                                    <use href="/icons.svg#icon-check1"></use>
                                 </svg>
-                                Професійна консультація</li>
+                                Професійна консультація
+                            </li>
                         </ul>
 
                         <div className={css.infoTrustReasons}>
                             <p className={css.infoTrustReason}>
-                                "Кожна піч, камін чи барбекю-комплекс — це не просто будівництво, а створення місця, де збирається родина. 
-                                Саме тому я приділяю особливу увагу якості матеріалів, безпеці конструкції та акуратності виконання робіт."
+                                <svg className={css.qouteIcon}>
+                                    <use href="/icons.svg#icon-quotes-left"></use>
+                                </svg>
                                 "За роки роботи я реалізував десятки проєктів різної складності, допомагаючи клієнтам створювати затишок і 
                                 тепло у своїх домівках."
+                                <svg className={css.qouteIcon}>
+                                    <use href="/icons.svg#icon-quotes-right"></use>
+                                </svg>
                             </p>
                         </div>
                     </div>               
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section className={css.hero + ' container'}>
             <div className={css.heroText}>
-                <h1 className={css.title}>Будую <span className={css.highlight}>печі, груби, каміни та барбекю-комплекси</span> під ключ</h1>
+                <h1 id="home" className={css.title}>Будую <span className={css.highlight}>печі, груби, каміни та барбекю-комплекси</span> під ключ</h1>
                 <h2 className={css.subtitle}>Відчуй теплий дотик домашнього затишку, створеного вогнем.</h2>
                 <h2 className={css.subtitle}>Тепло у вашому домі на довгі роки.</h2>
                 <div className={css.ctaButtons}>
